1860c323fe17ee92c897f4a42eaab6432a708f23
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2024-01-08 12:21:30 +0600

    fix: Minor Fix in Design

--- src/components/Banner/Navbar.jsx
+++ src/components/Banner/Navbar.jsx
@@ -6,16 +6,17 @@ const Navbar = ({ sidebar, setSidebar }) => {
     <div className="absolute top-0 left-0 w-full py-6 text-white z-20">
       <div className="container">
         <div className="flex justify-between items-center">
-          <h1 className="text-4xl font-bold uppercase ">
-            Mri<span className="font-normal text-2xl">Shot</span>
+          <h1 className="text-2xl font-bold uppercase ">
+            @<span className="font-normal text-xl">Sumonta056</span>
           </h1>
           <ul className="space-x-10 text-xl hidden lg:flex">
             <li>Home</li>
             <li>Projects</li>
-            <li>Achievment</li>
+            <li>Achievement</li>
+            {/* <li>About</li> */}
             <li>Photography</li>
             <li>Blogs</li>
-            <li>Contact</li>
+            {/* <li>Contact</li> */}
           </ul>
           <div onClick={() => setSidebar(!sidebar)}>
             <GiHamburgerMenu className="text-3xl cursor-pointer" />
