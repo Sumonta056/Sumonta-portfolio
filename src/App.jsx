46ee5045a1fe4ce53777846abf54ca71ad3e8fcb
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2024-04-04 22:43:42 +0600

    fix : Loading issue in Background

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React, { lazy } from "react";
 import { TinyButton as ScrollUpButton } from "react-scroll-up-button";
 import ScrollProgressBar from "react-scroll-progress-bar";
 import Banner from "./components/Banner/Banner";
-import Particle from "./components/Particle.js";
+const Particle = lazy(() => import("./components/Particle"));
 const Project = lazy(() => import("./components/Projects/Project"));
 
 function App() {
   return (
     <div className="bg-bodyColor">
       <ScrollProgressBar bgcolor="#d8d2db" height="7px" />
-      <Particle />
+      <React.Suspense
+        fallback={<div className="z-10 bg-black">Loading...</div>}
+      >
+        <Particle />
+      </React.Suspense>
       <Banner />
       <div id="project">
         <Project />
@@ -26,7 +30,6 @@ function App() {
           padding: "10px",
         }}
       />
-      
     </div>
   );
 }
